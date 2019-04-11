99fdd9db5a5459cd0b56ee6678f025f1b0732a2d
dediardiansyah <dediardiansah60@gmail.com>
2019-04-12 01:14:53 +0700

    content homepage #1

---
+++ js/reviews.js
@@ -0,0 +1,26 @@
+// Reviews slider
+    $(".reviews .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 35,
+        nav: true,
+        center: true,
+        autoplay: true,
+        navText: [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            750: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
